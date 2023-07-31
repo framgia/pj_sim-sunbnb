79346026fb51ab067252f1bb2087f5b0f37f90df
Lennard Uy <137018413+LennardUy104@users.noreply.github.com>
2023-07-31 10:42:47 +0800

    PJ_SIM_SUNBNB_2023-3 Setup Database (#6)
    * PJ_SIM_SUNBNB_2023-3 Setup Database
    
    * resolve Changes

---
+++ be/database/migrations/2023_07_21_023120_create_host_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('host', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('user_name');
+            $table->string('password');
+            $table->string('email');
+            $table->string('contact_num');
+            $table->text('photo_img_path')->nullable();
+            $table->text('id_img_path')->nullable();
+            $table->text('dti_img_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('host');
+    }
+};
---
+++ be/database/migrations/2023_07_21_023821_create_reservation_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('client')->cascadeOnDelete();
+            $table->foreignId('room_id')->constrained('room')->cascadeOnDelete();
+            $table->foreignId('host_id')->constrained('host')->cascadeOnDelete();
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->float('total_price');
+            $table->boolean('arrived')->default(false);
+            $table->boolean('is_approve')->default(false);
+            $table->boolean('is_cancel')->default(false);
+           
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
