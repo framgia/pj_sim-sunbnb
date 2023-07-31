7a61b44ca0f47f0c2a123753ca22d7e88682dc51
RonnieRII <ronnie.ramirez@sun-asterisk.com>
2023-07-31 14:46:56 +0800

    PJ_SIM_SUNBNB_2023-17-FE-Tab-Component
    PJ_SIM_SUNBNB_2023-17-FE-Tab-Component
    
    Added to AllComponents and functions to the tabs
    
    fix route index.js
    
    resolve changes

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -1,17 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AllComponents from "../pages/AllComponents.vue";
-import Registration from "../pages/RegistrationView.vue";
 import Login from "../pages/LogIn.vue";
 
 const routes = [
   {
     path: "/",
     component: AllComponents,
-  },
- {
-    path: "/registration",
-    component: Registration,
   },
   {
     path: "/login",
