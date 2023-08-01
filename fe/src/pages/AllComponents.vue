149620614a222743cf41870e966f26b5a630534c
Andre Enriquez <andre.enriquez@sun-asterisk.com>
2023-08-01 13:08:44 +0800

    Hotfix/Fix-routes
    Revert "Fix conflicts"
    
    This reverts commit e44a58db857a0422f05ef549a2af91a1c26c0ba9, reversing
    changes made to ca5e4ed29f3e98129eabcc739703b8fab6718eb1.
    
    Revert "Fix conflicts"
    
    This reverts commit e44a58db857a0422f05ef549a2af91a1c26c0ba9, reversing
    changes made to 326f96b8762703a1748eb861c8bade1b5880e8dc.
    
    Fix routes again
    
    Add small changes

---
+++ fe/src/pages/AllComponents.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HomepageBanner />
+    <Reservation />
+    <ReviewForm />
+    <ReviewRoom />
+    <ReviewHost />
+  </div>
+</template>
+
+<script>
+import Reservation from "../components/ReservationComponent.vue";
+import HomepageBanner from "../components/HomepageBanner.vue";
+import ReviewForm from "../components/Review/ReviewForm.vue";
+import ReviewRoom from "../components/Review/ReviewRoom.vue";
+import ReviewHost from "../components/Review/ReviewHost.vue";
+export default {
+  name: "AllComponents",
+  components: {
+    Reservation,
+    HomepageBanner,
+    ReviewForm,
+    ReviewRoom,
+    ReviewHost,
+  },
+};
+</script>
+
+<style></style>
